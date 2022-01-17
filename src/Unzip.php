b94bad697c62c897c6e524ff0c7bf29d58d61744
jameswade <james.wade@agentsoftware.net>
2022-01-17 16:41:49 +0000

    fix: cleanup

--- src/Unzip.php
+++ src/Unzip.php
@@ -2,11 +2,12 @@
 
 namespace Jpswade\LaravelDatabaseTools;
 
+use OutOfRangeException;
 use ZipArchive;
 
 class Unzip
 {
-    const ERRORS = [
+    public const ERRORS = [
         ZipArchive::ER_MULTIDISK => 'Multi-disk zip archives not supported.',
         ZipArchive::ER_RENAME => 'Renaming temporary file failed.',
         ZipArchive::ER_CLOSE => 'Closing zip archive failed',
@@ -32,7 +33,7 @@ class Unzip
         ZipArchive::ER_DELETED => 'Entry has been deleted',
     ];
 
-    function __invoke(string $file, string $path = '.'): ?array
+    public function __invoke(string $file, string $path = '.'): ?array
     {
         $errors = self::ERRORS;
         if (function_exists('zip_open') === false) {
@@ -43,30 +44,30 @@ class Unzip
         $file = realpath($file);
         if (is_writable($path) === false) {
             $message = "'$path' is not writable.";
-            throw new \OutOfRangeException($message);
+            throw new OutOfRangeException($message);
         }
         if (is_readable($file) === false) {
             $message = "'$file' is not readable.";
-            throw new \OutOfRangeException($message);
+            throw new OutOfRangeException($message);
         }
         $open = zip_open($file);
         if (is_resource($open) === false) {
             $error = ($errors[$open] ?? 'unknown');
             $message = sprintf("Unable to open zip '%s', error was '%s'.", $file, $error);
-            throw new \OutOfRangeException($message);
+            throw new OutOfRangeException($message);
         }
         $files = [];
         while ($entry = zip_read($open)) {
             $files[] = zip_entry_name($entry);
             zip_entry_open($open, $entry);
-            if (substr(zip_entry_name($entry), -1) == DIRECTORY_SEPARATOR) {
+            if (substr(zip_entry_name($entry), -1) === DIRECTORY_SEPARATOR) {
                 $dir = $path . DIRECTORY_SEPARATOR . substr(zip_entry_name($entry), 0, -1);
-                if (file_exists($dir) === false) {
-                    mkdir($dir);
+                if ((file_exists($dir) === false) && !mkdir($dir) && !is_dir($dir)) {
+                    throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
                 }
             } else {
                 $name = $path . DIRECTORY_SEPARATOR . zip_entry_name($entry);
-                $fh = fopen($name, 'w');
+                $fh = fopen($name, 'wb');
                 fwrite($fh, zip_entry_read($entry, zip_entry_filesize($entry)), zip_entry_filesize($entry));
                 fclose($fh);
             }
